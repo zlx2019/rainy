d7c5587ea92995ce5fd008b5e56cb829a58e9f68
zero9501 <15133024857@163.com>
2024-12-05 22:38:34 +0800

    feature: add redis list commands

--- rainy-services/rainy-sample/src/main/java/com/zero/rainy/sample/model/MailStorage.java
+++ rainy-services/rainy-sample/src/main/java/com/zero/rainy/sample/model/MailStorage.java
@@ -1,24 +1,35 @@
 package com.zero.rainy.sample.model;
 
-import lombok.Data;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 邮箱临时存储库
+ *
  * @author Zero.
- * <p> Created on 2024/12/5 10:44 </p>
+ * <p> Created on 2024/12/5 13:39 </p>
  */
-@Data
-public class MailStorage<V extends Map<String, ?>> {
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class MailStorage extends HashMap<String, MailMessage> implements Storage<MailMessage> {
     /**
-     * 邮箱账号
+     * 邮箱账号, 存储库的缓存Key
      */
-    private String key;
+    private String email;
 
-    /**
-     * 存储元素
-     */
-    private V elems;
+    @Override
+    public String getKey() {
+        return this.email;
+    }
+
+    @Override
+    public Map<String, MailMessage> getStore(){
+        return this;
+    }
+
 }
