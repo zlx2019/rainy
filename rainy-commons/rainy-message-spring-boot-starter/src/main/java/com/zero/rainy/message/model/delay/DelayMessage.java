9375ec04dfa50dd7ea7d47e531d94b836761db50
Zero <15133024857@163.com>
2025-01-09 18:35:10 +0800

    feat: message adjust

--- rainy-commons/rainy-message-spring-boot-starter/src/main/java/com/zero/rainy/message/model/delay/DelayMessage.java
+++ rainy-commons/rainy-message-spring-boot-starter/src/main/java/com/zero/rainy/message/model/delay/DelayMessage.java
@@ -1,6 +1,5 @@
 package com.zero.rainy.message.model.delay;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.zero.rainy.message.model.BaseMessage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,6 +20,5 @@ public abstract class DelayMessage extends BaseMessage {
     /**
      * 延迟时间, 默认为 3 秒
      */
-    @JsonIgnore
     private Duration delay = Duration.ofSeconds(3);
 }
